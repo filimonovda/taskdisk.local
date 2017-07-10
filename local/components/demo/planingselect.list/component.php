ce8fff720b46deb87e098709d069d87f45614572
Denis Filimonov <denis.filimonov@simbirsoft.ru>
2017-07-10 09:10:35 +0300

    gitignore

--- local/components/demo/planingselect.list/component.php
+++ local/components/demo/planingselect.list/component.php
@@ -2,45 +2,42 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
 use Bitrix\Disk\Folder;
+use Bitrix\Main\FileTable;
 
 $arParams["MainFolder"]=521;
 
-$RoomCount=Folder::getList(array("filter"=>array("=PARENT_ID"=>$arParams["MainFolder"])));
+$RoomCount=Folder::getList(array("filter"=>array("=PARENT_ID"=>$arParams["MainFolder"], "=TYPE"=>2, "=DELETED_TYPE"=>0)));
 while ($row = $RoomCount->fetch()){
     $RoomCountID[]=$row["ID"];
-    $arResult['SelectRoomCountList'][$row["NAME"]][$row["PARENT_ID"]][]=$row["ID"];
-
-    echo "<pre>";
-    //print_r(Folder::getList(array("filter"=>array("=PARENT_ID"=>$row["ID"])))->fetchAll());
-    echo "</pre>";
+    $arResult['SelectRoomCountList'][$row["ID"]]=$row["NAME"];
+    $arResult['SelectRoomCountList']=array_unique($arResult['SelectRoomCountList']);
 }
 
-$TypeHouse=Folder::getList(array("filter"=>array("=PARENT_ID"=>$RoomCountID)));
+$TypeHouse=Folder::getList(array("filter"=>array("=PARENT_ID"=>$RoomCountID, "=TYPE"=>2, "=DELETED_TYPE"=>0)));
 while ($row = $TypeHouse->fetch()){
     $TypeHouseID[]=$row["ID"];
-    $arResult['SelectTypeHouseList'][$row["NAME"]][$row["PARENT_ID"]][]=$row["ID"];
-
-    echo "<pre>";
-//    print_r($row);
-    echo "</pre>";
+    $arResult['SelectTypeHouseList'][$row["ID"]]=$row["NAME"];
+    $arResult['SelectTypeHouseList']=array_unique($arResult['SelectTypeHouseList']);
 }
 
-$AreaSize=Folder::getList(array("filter"=>array("=PARENT_ID"=>$TypeHouseID)));
+$AreaSize=Folder::getList(array("filter"=>array("=PARENT_ID"=>$TypeHouseID, "=TYPE"=>2, "=DELETED_TYPE"=>0)));
 while ($row = $AreaSize->fetch()){
-    $AreaSizeID[]=$row["ID"];
-    $arResult['SelectAreaSizeList'][$row["NAME"]][]=$row["ID"];
+    $AreaSizeID[] = $row["ID"];
+    $arResult['SelectAreaSizeList'][$row["ID"]] = $row["NAME"];
+    $arResult['SelectAreaSizeList'] = array_unique($arResult['SelectAreaSizeList']);
 
-    echo "<pre>";
-//    print_r($row);
-    echo "</pre>";
+    $VariantPlans=Folder::getList(array("filter"=>array("=PARENT_ID"=>$row["ID"], "=TYPE"=>3, "=DELETED_TYPE"=>0)));
+    while ($rowPlans = $VariantPlans->fetch()){
+        $arFile=FileTable::getById($rowPlans["FILE_ID"])->fetch();
+        $elTypeHouse=Folder::getList(array("select"=>array("ID","PARENT_ID","NAME"), "filter"=>array("=ID"=>$row["PARENT_ID"], "=TYPE"=>2, "=DELETED_TYPE"=>0),"limit"=>1))->fetch();
+        $elRoomCount=Folder::getList(array("select"=>array("ID","PARENT_ID","NAME"), "filter"=>array("=ID"=>$elTypeHouse["PARENT_ID"], "=TYPE"=>2, "=DELETED_TYPE"=>0),"limit"=>1))->fetch();
+        $SmilePicURL="/upload/".$arFile["SUBDIR"]."/".$arFile["FILE_NAME"];//FileTable::getById($rowPlans["FILE_ID"])->fetch(), Array("width" => 150, "height" => 300);
+        $arResult['SelectVariantPlansList'][]=array("ID"=>$rowPlans["FILE_ID"], "NAME"=>$rowPlans["NAME"], "RoomCount"=>$elRoomCount["ID"], "TypeHouse"=>$elTypeHouse["ID"], "AreaSize"=>$row["ID"], "SmilePicURL"=>$SmilePicURL);
+    }
 }
 
-//$RoomCountID[]=Folder::getList(array("select"=>array("ID"),"filter"=>array("=PARENT_ID"=>$arParams["MainFolder"])))->fetch();
-
-
-
-echo "<pre>";
-print_r($arResult);
-echo "</pre>";
+//echo "<pre>";
+//print_r($arResult);
+//echo "</pre>";
 $this->IncludeComponentTemplate($componentPage);
 ?>
--- local/components/demo/planingselect.list/templates/.default/template.php
+++ local/components/demo/planingselect.list/templates/.default/template.php
@@ -13,22 +13,52 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
 /** @var \Bitrix\Disk\Internals\BaseComponent $component */
 use Bitrix\Main\Localization\Loc;
 ?>
+<script language="JavaScript">
+    function selectadditems(selectObject) {
+        var nextid=selectObject.id; //атрибут
+        var parentid=selectObject.options[selectObject.selectedIndex].value; //значение
 
-<select name="SelectRoomCountList" id="SelectRoomCountList">
-<?foreach ($arResult['SelectRoomCountList'] as $key => $value):?>
-    <?print_r($value)?>
-    <option  onselect=""><?=$key?></option>
-<?endforeach;?>
-</select>
+        var el = document.querySelector("div#imgplains div["+nextid+"="+parentid+"]");
+        //$("div[nextid*='parentid']").className="disabled";
+        //$("div:not([nextid*='parentid'])").className="enable";
 
-<select name="SelectTypeHouseList" id="SelectTypeHouseList">
+            alert(el.className);
+            //alert(selectObject.options[selectObject.selectedIndex].value);
+
+            //var selectBox = document.getElementById(nextID);
+            //var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+
+
+            //document.getElementById('test').innerHTML = ""; // очищаем Select
+            //document.getElementById('sel_1').options.length = 0; // еще один способ очистки, найденный в интернете
+            //document.getElementById('test').innerHTML = "<select id='sel_1'><option>Воронеж</option><option>Новгород</option></select>"; // вставляем новые option'ы
+    }
+</script>
+
+<div>
+    <select name="SelectRoomCountList" id="SelectRoomCountList" onchange="javascript:selectadditems(this)">
+        <option value="all">Все</option>
+    <?foreach ($arResult['SelectRoomCountList'] as $key => $value):?>
+        <option value="<?=$key?>"><?=$value?></option>
+    <?endforeach;?>
+    </select>
+
+    <select name="SelectTypeHouseList" id="SelectTypeHouseList" onchange="javascript:selectadditems(this)">
+        <option value="all">Все</option>
     <?foreach ($arResult['SelectTypeHouseList'] as $key => $value):?>
-        <option onselect=""><?=$key?></option>
+        <option value="<?=$key?>"><?=$value?></option>
     <?endforeach;?>
-</select>
+    </select>
 
-<select name="SelectAreaSizeList" id="SelectAreaSizeList">
+    <select name="SelectAreaSizeList" id="SelectAreaSizeList" onchange="javascript:selectadditems(this)">
+        <option value="all">Все</option>
     <?foreach ($arResult['SelectAreaSizeList'] as $key => $value):?>
-        <option onselect=""><?=$key?></option>
+        <option value="<?=$key?>"><?=$value?></option>
     <?endforeach;?>
-</select>
+    </select>
+</div>
+<div id="imgplains">
+<?foreach ($arResult['SelectVariantPlansList'] as $arResult):?>
+    <div class="disabled" style="float: left; padding: 10px;" SelectRoomCountList="<?=$arResult["RoomCount"]?>" SelectTypeHouseList="<?=$arResult["TypeHouse"]?>" SelectAreaSizeList="<?=$arResult["AreaSize"]?>"><img src="<?=$arResult["SmilePicURL"]?>" style="max-height: 300px;"></div>
+<?endforeach;?>
+</div>
