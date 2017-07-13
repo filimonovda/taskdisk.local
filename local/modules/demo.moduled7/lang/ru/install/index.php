77db40b2a65b5efe9c997f4c0eab38010afcc35d
Denis Filimonov <denis.filimonov@simbirsoft.ru>
2017-07-13 11:27:23 +0300

    модуль

---
+++ local/modules/demo.moduled7/lang/ru/install/index.php
@@ -0,0 +1,6 @@
+<?
+$MESS["demo.moduled7_MODULE_NAME"] = "Demo module D7";
+$MESS["demo.moduled7_MODULE_DESC"] = "Description demo module D7";
+$MESS["demo.moduled7_PARTNER_NAME"] = "demo";
+$MESS["demo.moduled7_PARTNER_URI"] = "http://www.mysite.ru";
+?>
