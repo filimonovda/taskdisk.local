45e906b2070f03ec5fb4fc7ef60193846ca4a013
Denis Filimonov <denis.filimonov@simbirsoft.ru>
2017-07-13 16:08:45 +0300

    инсталятор

--- test.php
+++ test.php
@@ -1,26 +1,44 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-//$group = new CGroup;
-/*$user = new CUser;
-$arFields = Array(
-    "NAME"              => "Сергей",
-    "LAST_NAME"         => "Иванов",
-    "EMAIL"             => "ivanov@microsoft.com",
-    "LOGIN"             => "ivan",
-    "LID"               => "ru",
-    "ACTIVE"            => "Y",
-    "GROUP_ID"          => array(10,11),
-    "PASSWORD"          => "123456",
-    "CONFIRM_PASSWORD"  => "123456"
-);*/
 
-//$ID = $user->Add($arFields);
-//$ID = $group->Add($arFields);
-//echo $ID;
+use \Bitrix\Main\Loader,
+	Demo\Moduled7,
+	Bitrix\Highloadblock\HighloadBlockTable as HLBTb,
+	Bitrix\Main\Entity;
 
-use Bitrix\Main\GroupTable;
+Loader::includeSharewareModule("demo.moduled7"); 
+Loader::includeSharewareModule("highloadblock"); 
 
-GroupTable::add(array(
-        "NAME"=>"test"
-       ));
+$HLGetList = HLBTb::GetList(array("filter" => array('TABLE_NAME' => 'testhi')))->Fetch();
+$HLEntity = HLBTb::compileEntity($HLGetList);
+$HLGetListHeight = HLBTb::GetList(array("filter" => array('TABLE_NAME' => 'Height')))->Fetch();
+$HLEntityHeight = HLBTb::compileEntity($HLGetListHeight);
+
+$arResult = Demo\Moduled7\Moduled7Table::GetList(
+	array(
+		'select' => array('USERID' => 'ID', 'VALERANAME' => 'USER.LOGIN', 'COLOR' => 'HLCOLOR.UF_COLOR', 'COUNT', 'MAX', 'SUM'),
+		'runtime' => array(
+                new Entity\ReferenceField(
+                    'HLCOLOR',
+                    $HLEntity,
+                    ['this.ID' => 'ref.UF_USER_ID']
+                ),
+				new Entity\ReferenceField(
+                    'HLHeight',
+                    $HLEntityHeight,
+                    ['=this.ID' => 'ref.UF_USER_ID']
+                ),
+				new Entity\ExpressionField('COUNT', 'COUNT(%s)', array('HLHeight.UF_VALUE')),
+				new Entity\ExpressionField('MAX', 'MAX(%s)', array('HLHeight.UF_VALUE')),
+				new Entity\ExpressionField('SUM', 'SUM(%s)', array('HLHeight.UF_VALUE')),
+            ),
+	)
+);
+
+echo "<pre>";
+print_r($arResult->fetchall());
+echo "</pre>";
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
 ?>
+
