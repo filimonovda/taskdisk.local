fc5aadde8b0be766418c3e9714cff24331809547
Denis Filimonov <denis.filimonov@simbirsoft.ru>
2017-07-18 14:56:27 +0300

    конпозит+кеш

---
+++ test/index2.php
@@ -0,0 +1,11 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Title");
+?><?
+$APPLICATION->IncludeComponent(
+	"demo:myfirst",
+	".default", 
+	array(),
+	false
+);
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
