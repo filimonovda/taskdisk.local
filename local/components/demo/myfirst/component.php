80427c49aa73beb834ce20af3aa3c7631421c0d8
Denis Filimonov <denis.filimonov@simbirsoft.ru>
2017-07-18 14:55:28 +0300

    конпозит+кеш

--- local/components/demo/myfirst/component.php
+++ local/components/demo/myfirst/component.php
@@ -2,6 +2,12 @@
 
 use Bitrix\Crm\DealTable;
 
-DealTable::getList()
+if(isset($_REQUEST["ID"]))
+    $List = DealTable::getList(array("filter"=>array("ID" => $_REQUEST["ID"])));
+else
+    $List = DealTable::getList(array());
+endif;
 
+$arResult = $List->fetchAll();
+$this->IncludeComponentTemplate($componentPage);
 ?>
